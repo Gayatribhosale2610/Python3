46e2a73855ce292534a9b86d5e4c99bd2c8db6c5
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-09 23:34:46 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,2 +1,18 @@
 ## Day 1
 
+### Create list and displayed with append().
+
+print("By using append() method")
+lst = []
+num = int(input("Enter list element : \n"))
+for i in range(0,num):
+    a = int(input())
+    lst.append(a)
+print("The appended list is : \n",lst)
+
+print("without using append() method")
+x = [21,32,43]
+y = x + ['hello','welcome']
+print("x is : \n",x)
+print("appended list is : \n",y)
+ 
