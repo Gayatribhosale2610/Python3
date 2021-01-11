2d2dd4f9bc6f4fab4dd39424d8904f2f05469af4
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-11 22:49:15 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -1,1 +1,16 @@
+## Day 4
 
+#### A program to check a list is empty or not.
+
+lst =[]
+n = int(input("Enter list number:\n"))
+for i in range(n):
+    a = int(input())
+    lst.append(a)
+print(lst)
+if not lst:
+    print("List is empty")
+else:
+    print("List is not empty")
+   
+  -----------------------------------------------------------
