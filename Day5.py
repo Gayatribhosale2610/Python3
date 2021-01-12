3a3a38e2d06e118738a400b6217fb5b354ca2007
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-12 23:49:27 +0530

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -11,6 +11,7 @@ print ("list2:", list2)
 print ("Difference elements:")
 print (list (set(list1) - set (list2)))
 
+--------------------------------------------------------------------
 
 #### 3.A program to convert a list of characters into a string. 
 
