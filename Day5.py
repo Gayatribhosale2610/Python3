d1533607c3eb62d892379f82b9c10881f6e6b05a
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-12 23:47:42 +0530

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -1,1 +1,13 @@
+## Day 5
+
+#### 2.A program to get the difference between the two lists.
+
+list1 = [10, 20, 30, 40, 50]
+list2 = [10, 20, 30, 60, 70]
+
+print ("list1:", list1)
+print ("list2:", list2)
+
+print ("Difference elements:")
+print (list (set(list1) - set (list2)))
 
