60971ec1caf794b84b72e4c2bf030626f5f35371
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-13 23:24:13 +0530

    Update Day6.py

--- Day6.py
+++ Day6.py
@@ -10,3 +10,23 @@ for i in range(int(number1)):
     list1.append(l)
 print("list =",list1)
 print("unique values =",list(set(list1)))
+
+#### Program to check whether two list are circularly identical or not..
+
+A=[]
+n=int(input("length of list:"))
+print("elements of first list:")
+for i in range(int(n)):
+   k=int(input(""))
+   A.append(k)
+
+B=[]
+n1=int(input("length of list:"))
+print("Element of the Second List:")
+for i in range(int(n1)):
+   k=int(input(""))
+   B.append(k)
+if ' '.join(map(str, B)) in ' '.join(map(str, A * 2)):
+   print("lists are circularly identical")
+else:
+    print("lists are not circularly identical")
