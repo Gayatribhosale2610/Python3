43620f59dd550e4d344a28ac289ee861b7569f95
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-13 23:22:52 +0530

    Update Day6.py

--- Day6.py
+++ Day6.py
@@ -2,3 +2,11 @@
   
   #### program to get unique values from a list
 
+list1 =[]
+number1=int(input("enter length of list"))
+print("enter element")
+for i in range(int(number1)):
+    l = int(input(""))
+    list1.append(l)
+print("list =",list1)
+print("unique values =",list(set(list1)))
