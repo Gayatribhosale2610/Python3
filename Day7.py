a0f03c6c4ee3524430422219c9a7bf7d3445c660
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-14 23:41:21 +0530

    Update Day7.py

--- Day7.py
+++ Day7.py
@@ -15,3 +15,5 @@ for i in lst:
     else:
         freq[i] = 1
 print("frequency of elements in a list is :  "+str(freq))
+
+#### 2. write a  program to count the number of elements in a list within a specified range.
