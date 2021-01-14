9526ca9197ce34544dabdd677557f3e7bd805198
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-14 23:41:57 +0530

    Update Day7.py

--- Day7.py
+++ Day7.py
@@ -17,3 +17,19 @@ for i in lst:
 print("frequency of elements in a list is :  "+str(freq))
 
 #### 2. write a  program to count the number of elements in a list within a specified range.
+
+list1 = []
+c = 0
+n = int(input("list elements \n"))
+for i in range(n):
+    a = int(input())
+    list1.append(a)
+print("List is :"+str(list1))
+
+min = int(input("Enter the range\n"))
+max = int(input("Enter the range\n"))
+for j in list1:
+    if j >= min and j <= max:
+        c += 1
+print("count the number of elements in a list within a range "
+      +str(min) +" to "+str(max)+ " is  : " +str(c))
