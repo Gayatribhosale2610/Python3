a688e4727d9c8ced5aee326a227bad253287ccb4
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-14 23:40:46 +0530

    Update Day7.py

--- Day7.py
+++ Day7.py
@@ -1,3 +1,17 @@
 ## Day 7
 
 #### 1. Write a Python program to get the frequency of the elements in a list.
+
+lst = []
+freq = {}
+n = int(input("Enter number of list\n"))
+for i in range(n):
+    a = int(input())
+    lst.append(a)
+print(lst)
+for i in lst:
+    if i in freq:
+        freq[i] += 1
+    else:
+        freq[i] = 1
+print("frequency of elements in a list is :  "+str(freq))
