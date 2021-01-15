7de6fea9bf8ff84a630715dfed95be9376a94a42
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-15 22:26:31 +0530

    Update Day8.py

--- Day8.py
+++ Day8.py
@@ -9,4 +9,18 @@ for i in range(n):
     value = int(input("value\n"))
     dic[key] = value
 print(dic)
+
+#### 2. Write a Program to add a key to a dictionary.
     
+dic = {}
+n = int(input("Enter dictionary element number\n"))
+for i in range(n):
+    key = input("key\n")
+    value = int(input("value\n"))
+    dic[key] = value
+print("Original Dictionary : "+str(dic))
+key=input("Enter the key\n")
+value=int(input("Enter the value\n"))
+dic.update({key:value})
+print("Updated dictionary is :\n"+str (dic))
+
