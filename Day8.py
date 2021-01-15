078964a832df493a16490c257720d1c2d07ffa89
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-15 22:18:01 +0530

    Update Day8.py

--- Day8.py
+++ Day8.py
@@ -1,2 +1,12 @@
 ## Day 8
 
+#### 1. Write a program to create dictionary and access all elements with keys and values.
+
+dic = {}
+n = int(input("Enter dictionary element number\n"))
+for i in range(n):
+    key = input("key\n")
+    value = int(input("value\n"))
+    dic[key] = value
+print(dic)
+    
