f1c5a3230ef950454c56ff47687c9d9141f00e74
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-17 23:36:21 +0530

    Update Day10.py

--- Day10.py
+++ Day10.py
@@ -1,2 +1,20 @@
 ## Day 10
 #### 1.Write a program to concatenate following dictionaries to create a new one. 
+
+def dic1():
+    d={}
+    n=int(input("Enter elements\n"))
+    for i in range(n):
+        
+        key=int(input("key\n"))
+        value=int(input("value\n"))
+        d[key]=value
+    return d
+d1=dic1()
+d2=dic1()
+d3=dic1()
+print("dictionary 1=",d1,"\ndictionary 2=",d2,"\ndictionary 3=",d3)
+dic4={}
+for i in (d1,d2,d3):
+    dic4.update(i)
+print("Concatenated dictionary is: ",dic4)
