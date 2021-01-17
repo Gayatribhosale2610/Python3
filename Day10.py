aac50dafb65a5499352673adacabf7ce9a5730a5
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-17 23:38:25 +0530

    Update Day10.py

--- Day10.py
+++ Day10.py
@@ -1,2 +1,13 @@
 ## Day 10
 #### 1.Write a program to remove a key from a dictionary.
+dic1 = {}
+a = int(input("Enter dictionary element\n"))
+for i in range(a):
+    key = input("key\n")
+    value = int(input("value\n"))
+    dic1[key] = value
+print(dic1)
+x = input("Enter key which you have to delete\n")
+if x in dic1:
+    del dic1[x]
+print(dic1)
