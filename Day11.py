64a30066ebd16d37a999e359f1a917d8cf3cd43f
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-18 23:28:48 +0530

    Update Day11.py

--- Day11.py
+++ Day11.py
@@ -12,3 +12,13 @@ print("dictionary =",dict)
 print("unique values in a dictionary =",set(dict.values()))
 
 #### 2.Write a  program to find the highest 3 values in a dictionary.
+
+dict ={}
+n=int(input("Enter elements\n"))
+for i in range(n):
+     key =(input ("key\n"))
+     value = int(input("value\n"))
+     dict[key] = value
+print ("Dictionary is : ", dict)
+list = sorted (set (dict.values()))
+print ("highest 3 values in dictionary : ", set (list [-3 :]))
