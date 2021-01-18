ce3779204e6391f2dfeb47387c243d5aeaefba2c
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-18 23:33:43 +0530

    Update Day11.py

--- Day11.py
+++ Day11.py
@@ -11,19 +11,7 @@ for i in range(n):
 print("dictionary =",dict)
 print("unique values in a dictionary =",set(dict.values()))
 
-#### 2.Write a  program to find the highest 3 values in a dictionary.
-
-dict ={}
-n=int(input("Enter elements\n"))
-for i in range(n):
-     key =(input ("key\n"))
-     value = int(input("value\n"))
-     dict[key] = value
-print ("Dictionary is : ", dict)
-list = sorted (set (dict.values()))
-print ("highest 3 values in dictionary : ", set (list [-3 :]))
-
-#### Write a program to create and display all combinations of letters, selecting each letter from a different kay in a dictionary. 
+#### 2.Write a program to create and display all combinations of letters, selecting each letter from a different kay in a dictionary. 
 dict={}
 n=int(input("Enter Elements\n"))
 for i in range(n):
@@ -37,3 +25,17 @@ for i in list1:
         for a in list1[j]:
             dict1 =i+a
             print(dict1)
+
+
+#### 3.Write a  program to find the highest 3 values in a dictionary.
+
+dict ={}
+n=int(input("Enter elements\n"))
+for i in range(n):
+     key =(input ("key\n"))
+     value = int(input("value\n"))
+     dict[key] = value
+print ("Dictionary is : ", dict)
+list = sorted (set (dict.values()))
+print ("highest 3 values in dictionary : ", set (list [-3 :]))
+
