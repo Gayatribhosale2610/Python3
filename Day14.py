3c68d546de25b8822208b7e0ef6d8ade452dc566
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-19 23:48:39 +0530

    Update Day14.py

--- Day14.py
+++ Day14.py
@@ -1,2 +1,13 @@
 ## Day 14
 #### 1.Write a  program to remove spaces from dictionary keys.
+dic = {}
+n = int(input("Enter dictionary element number\n"))
+for i in range(n):
+    key = input("key\n")
+    value = int(input("value\n"))
+    dic[key] = value
+print(dic)
+  
+dic = {x.replace(' ', ''): v  
+     for x, v in dic.items()} 
+print (" New dictionary : ", dic)
