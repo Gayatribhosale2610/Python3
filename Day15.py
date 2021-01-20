b86584b5c591a80b452f5ee5edd32237344cd107
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-20 22:04:54 +0530

    Update Day15.py

--- Day15.py
+++ Day15.py
@@ -1,2 +1,9 @@
 ## Day 15
 #### 1.Write a program to print a dictionary line by line.
+dic={}
+n=int(input("enter element\n"))
+for i in range(n):
+    key=input("key\n")
+    value=input("value\n")
+    dic[key]=value
+print(dic)
