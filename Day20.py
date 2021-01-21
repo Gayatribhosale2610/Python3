b4885aa4c472d4551a6b06233efa7542756861d2
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-21 21:06:40 +0530

    Update Day20.py

--- Day20.py
+++ Day20.py
@@ -10,3 +10,8 @@ for n in str1:
 print(freq)
 
 #### Write a  program to get a string from a given string where all occurrences of its first char have been changed to '$', except the first char itself. 
+string = input("Enter the string\n")
+a = string[0]
+string = string.replace(a,'$')
+string = a + string[1:]
+print("All occurrences of its first char have been changed to '$'\ : ",string)
