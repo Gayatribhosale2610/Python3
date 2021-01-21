3fff7e385437dd83c9f51930da0378df9e44efbd
Gayatri Bhosale <65784117+Gayatribhosale2610@users.noreply.github.com>
2021-01-21 21:05:27 +0530

    Update Day20.py

--- Day20.py
+++ Day20.py
@@ -1,2 +1,10 @@
 ## Day 20
 #### Write a  program to count the number of characters (character frequency) in a string
+freq = {}
+str1 = input("Enter string : ")
+for n in str1:
+    if n in freq:
+        freq[n] += 1
+    else:
+        freq[n] = 1
+print(freq)
